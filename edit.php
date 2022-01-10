1d373d80db73f5d4360b758fa24be8d5a74bcf6e
rajaa elqasemy <elqasemyrajaa@gmail.com>
2022-01-10 23:10:20 +0100

    commit

---
+++ edit.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Project Worlds || DASHBOARD </title>
+<link  rel="stylesheet" href="css/bootstrap.min.css"/>
+ <link  rel="stylesheet" href="css/bootstrap-theme.min.css"/>    
+ <link rel="stylesheet" href="css/main.css">
+ <link  rel="stylesheet" href="css/font.css">
+ <script src="js/jquery.js" type="text/javascript"></script>
+
+  <script src="js/bootstrap.min.js"  type="text/javascript"></script>
+ 	<link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+
+<script>
+$(function () {
+    $(document).on( 'scroll', function(){
+        console.log('scroll top : ' + $(window).scrollTop());
+        if($(window).scrollTop()>=$(".logo").height())
+        {
+             $(".navbar").addClass("navbar-fixed-top");
+        }
+
+        if($(window).scrollTop()<$(".logo").height())
+        {
+             $(".navbar").removeClass("navbar-fixed-top");
+        }
+    });
+});</script>
+</head>
+
+<body  style="background:#eee;">
+<div class="header">
+<div class="row">
+<div class="col-lg-6">
+<span class="logo">Test Your Skill</span></div>
+<?php
+ include_once 'dbConnection.php';
+session_start();
+$email=$_SESSION['email'];
+  if(!(isset($_SESSION['email']))){
+header("location:index.php");
+
+}
+else
+{
+$name = $_SESSION['name'];;
+
+include_once 'dbConnection.php';
+echo '<span class="pull-right top title1" ><span class="log1"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>&nbsp;&nbsp;&nbsp;&nbsp;Hello,</span> <a href="account.php" class="log log1">'.$name.'</a>&nbsp;|&nbsp;<a href="logout.php?q=account.php" class="log"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>&nbsp;Signout</button></a></span>';
+}?>
+
+</div></div>
+<!-- admin start-->
+
+<!--navigation menu-->
+<nav class="navbar navbar-default title1">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="dash.php?q=0"><b>Dashboard</b></a>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li <?php if(@$_GET['q']==0) echo'class="active"'; ?>><a href="dash.php?q=0">Home<span class="sr-only">(current)</span></a></li>
+        <li <?php if(@$_GET['q']==1) echo'class="active"'; ?>><a href="dash.php?q=1">User</a></li>
+		<li <?php if(@$_GET['q']==2) echo'class="active"'; ?>><a href="dash.php?q=2">Ranking</a></li>
+		<li <?php if(@$_GET['q']==3) echo'class="active"'; ?>><a href="dash.php?q=3">Feedback</a></li>
+        <li class="dropdown <?php if(@$_GET['q']==4 || @$_GET['q']==5) echo'active"'; ?>">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Quiz<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="dash.php?q=4">Add Quiz</a></li>
+            <li><a href="dash.php?q=5">Remove Quiz</a></li>
+			
+          </ul>
+        </li><li class="pull-right"> <a href="logout.php?q=account.php"><span class="glyphicon glyphicon-log-out" aria-hidden="true"></span>&nbsp;&nbsp;&nbsp;&nbsp;Signout</a></li>
+		
+      </ul>
+          </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+<!--navigation menu closed-->
+<div class="container"><!--container start-->
+<div class="row">
+<div class="col-md-12">
+
+<!--add quiz start-->
+<?php
+
+
+$eid=@$_GET['eid'];
+
+$result = mysqli_query($con,"SELECT * FROM quiz WHERE eid = '$eid'") or die('Error');
+$count=mysqli_num_rows($result);
+if($count==1){
+while($row = mysqli_fetch_array($result)) {
+	  $title = $row['title'];
+      $total = $row['total'];
+      $sahi = $row['sahi'];
+      $wrong = $row['wrong'];
+      $time = $row['time'];
+      $tag = $row['tag']; 
+      $intro=$row['intro'];
+}
+
+
+}
+echo ' 
+<div class="row">
+<span class="title1" style="margin-left:40%;font-size:30px;"><b>Edit Quiz Details</b></span>
+<br /><br />
+ <div class="col-md-3"></div><div class="col-md-6">   <form class="form-horizontal title1" name="form"   method="POST">
+<fieldset>
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="name"></label>  
+  <div class="col-md-12">
+  <input id="name" name="title" placeholder="Enter Quiz title" value="'.$title.'" class="form-control input-md" type="text">
+    
+  </div>
+</div>
+
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="total"></label>  
+  <div class="col-md-12">
+  <input id="total" name="total" value="'.$total.'" placeholder="Enter total number of questions" class="form-control input-md" type="number">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="right"></label>  
+  <div class="col-md-12">
+  <input id="right" name="sahi" value="'.$sahi.'" placeholder="Enter marks on right answer" class="form-control input-md" min="0" type="number">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="wrong"></label>  
+  <div class="col-md-12">
+  <input id="wrong"  value="'.$wrong.'" name="wrong" placeholder="Enter minus marks on wrong answer without sign" class="form-control input-md" min="0" type="number">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="time"></label>  
+  <div class="col-md-12">
+  <input id="time" name="time" value="'.$time.'" placeholder="Enter time limit for test in minute" class="form-control input-md" min="1" type="number">
+    
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="tag"></label>  
+  <div class="col-md-12">
+  <input id="tag" name="tag" value="'.$tag.'" placeholder="Enter #tag which is used for searching" class="form-control input-md" type="text">
+    
+  </div>
+</div>
+
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-12 control-label" for="desc"></label>  
+  <div class="col-md-12">
+  <textarea rows="8" cols="8" name="intro" value="'.$intro.'" class="form-control" placeholder="Write description here..."></textarea>  
+  </div>
+</div>
+
+
+<div class="form-group">
+  <label class="col-md-12 control-label" for=""></label>
+  <div class="col-md-12"> 
+  <button name="update" type="submit" style="margin-left:45%" class="btn btn-primary glyphicon glyphicon-edit">Edit</button>
+ 
+
+  </div>
+</div>
+
+</fieldset>
+</form></div>';
+
+
+
+?>
+<?php
+if(isset($_POST['update'])) // when click on Update button
+{
+     $eid=@$_GET['eid'];
+    $title = $_POST['title'];
+    $total = $_POST['total'];
+    $sahi = $_POST['sahi'];
+    $wrong = $_POST['wrong'];
+    $time = $_POST['time'];
+    $tag = $_POST['tag'];
+    $intro = $_POST['intro'];
+	
+    $edit = mysqli_query($con,"update quiz set title='$title',total='$total',sahi='$sahi',wrong='$wrong',time='$time',tag='$tag',intro='$intro' where eid='$eid'");
+	
+    if($edit)
+    {
+      echo "<script type=\"text/javascript\">
+          alert('operation well done')
+          </script>";
+    }
+     	
+}
+
+?>
+
+</div><!--container closed-->
+</div></div>
+</body>
+</html>
